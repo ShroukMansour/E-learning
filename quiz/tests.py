2e8904ac4b5230e7d6aede9720c8b80ab81a9748
Gamal Khaled <Gamal.Khaled11@gmail.com>
2019-04-27 15:32:14 +0200

    Update tests.py

--- quiz/tests.py
+++ quiz/tests.py
@@ -34,7 +34,7 @@ class QuestionTest(APITestCase):
         response = self.client.post(url, data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Question.objects.count(), 1)
-        self.assertEqual(Question.objects.get().question_text, "what are the most famous type of inheritance?")
+        self.assertEqual(Question.objects.get().question_text, "what are the most famous type of inheritance")
 
 
     def test_post_question_with_multiple_correct_answer(self):
