0f7d5de04d1efa37c65b25fb5e0ee937262992db
ShroukMansour <shroukmansour99@gmail.com>
2019-04-28 20:33:56 +0200

    Make build.sh executable

--- merge_script.sh
+++ merge_script.sh
@@ -4,6 +4,7 @@
 
 export GIT_COMMITTER_EMAIL='shroukmansour99@gmail.com'
 export GIT_COMMITTER_NAME='ShroukMansour'
+export GIT_COMMITTER_PASSWORD='da75664bca798621d5a3b8398d44465a7ee2c06f'
 
 printf '1>>>>>>>>>>>>>.\n'
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
